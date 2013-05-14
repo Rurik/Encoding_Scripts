f9b74505610a1206d580b6ea0f1ba0526a7a0e69
Rurik <brian@thebaskins.com>
2013-05-14 03:05:18 -0300

    Bug fix
    Had errant tabs and case issue

--- xor_skipnull.py
+++ xor_skipnull.py
@@ -1,15 +1,15 @@
 import sys
 
 def xor_skipnull(data, xor):
-	result = ""
-	for i in range(0,len(data)):
-		char = data[i]
-		if char != "\x00" and char != xor: 
+    result = ""
+    for i in range(0,len(data)):
+        char = data[i]
+        if char != "\x00" and char != xor: 
             char = chr(ord(char) ^ ord(xor))
-		result += char
-	return result
+        result += char
+    return result
 
-if __NAME__ == "__main__":
+if __name__ == "__main__":
     
     if len(sys.argv) < 3:
         print "XOR Skip Null. Version 1.1 - @bbaskin"
@@ -17,7 +17,7 @@ if __NAME__ == "__main__":
         print "Input : Raw Data File, hex value"
         print "Output: Data file with each byte XOR'd by key, skipping nulls"
         quit()
-	
+
     filename = sys.argv[1]
     xor = chr(int(sys.argv[2],16))
     print "Using XOR 0x%s" % xor.encode("hex")
